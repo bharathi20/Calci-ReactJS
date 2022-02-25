128ed84eb3eea32a29b788222dd0e1387f4f9a6d
Bharathi <vbharu20@gmail.com>
2022-02-26 00:23:08 +0530

    calci project added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='calculator'>
 
-export default App;
+      <div className="topContainer">
+        <div className="time">
+          <p>9:41</p>
+        </div>
+
+        <div className="space">
+          <p></p>
+        </div>
+
+        <div className="status">
+          <p>battery</p>
+        </div> 
+
+      </div>
+
+      <div className="value">
+        <p>0</p>
+      </div>
+
+      <div className='buttonsContainer'>
+        <p className='button grey'>AC</p>
+        <p className='button grey'>C</p>
+        <p className='button grey'>%</p>
+        <p className='button operator'>/</p>
+        <p className='button'>7</p>
+        <p className='button'>8</p>
+        <p className='button'>9</p>
+        <p className='button operator'>*</p>
+        <p className='button'>4</p>
+        <p className='button'>5</p>
+        <p className='button'>6</p>
+        <p className='button operator'>-</p>
+        <p className='button'>1</p>
+        <p className='button'>2</p>
+        <p className='button'>3</p>
+        <p className='button operator'>+</p>
+        <p className='button shape'>0</p>
+        <p className='button'>.</p>
+        <p className='button operator'>=</p>
+      </div>
+    </div>
+
+  )
+}
